4e75ce04e4b338e0cdc66330a0fb7b45f7658997
Sourav Sarkar <samjhonson932@gmail.com>
2024-11-12 14:21:11 +0530

    Added Basic fetch

--- index.js
+++ index.js
@@ -23,6 +23,140 @@ app.get('/', (req, res) => {
 });
 
 
+// Functions to fetch data
+
+async function fetchGamesById(id) {
+    try{
+        const data = await Game.findById(id)
+        return data
+    }catch(eror)
+    {
+        throw error
+    }
+}
+
+async function fetchPhoneById(id) {
+    try{
+        const data = await Phone.findById(id)
+        return data
+    }catch(eror)
+    {
+        throw error
+    }
+}
+
+async function fetchJacketById(id) {
+    try{
+        const data = await Jacket.findById(id)
+        return data
+    }catch(eror)
+    {
+        throw error
+    }
+}
+async function fetchBookById(id) {
+    try{
+        const data = await Book.findById(id)
+        return data
+    }catch(eror)
+    {
+        throw error
+    }
+}
+
+// Api to fetch products
+
+
+app.get('/games/:id',async (req,res) =>{
+    try{
+        const data = await fetchGamesById(req.params.id)
+        res.status(200).json({message:"Success",data:data})
+    } catch(error)
+    {
+        console.log(error)
+        res.status(404).json({error:"Not Found"})
+    }
+})
+
+app.get('/games',async (req,res) =>{
+    try{
+        const games = await Game.find()
+        res.status(200).json({message:"Success",data: games})
+    }
+    catch(error)
+    {
+        res.status(404).json({error:"Not Found"})
+    }
+})
+
+
+app.get('/phone/:id',async (req,res) =>{
+    try{
+        const data = await fetchPhoneById(req.params.id)
+        res.status(200).json({message:"Success",data:data})
+    } catch(error)
+    {
+        console.log(error)
+        res.status(404).json({error:"Not Found"})
+    }
+})
+
+app.get('/phone',async (req,res) =>{
+    try{
+        const games = await Game.find()
+        res.status(200).json({message:"Success",data: games})
+    }
+    catch(error)
+    {
+        res.status(404).json({error:"Not Found"})
+    }
+})
+
+
+app.get('/jacket/:id',async (req,res) =>{
+    try{
+        const data = await fetchJacketById(req.params.id)
+        res.status(200).json({message:"Success",data:data})
+    } catch(error)
+    {
+        console.log(error)
+        res.status(404).json({error:"Not Found"})
+    }
+})
+app.get('/jacket',async (req,res) =>{
+    try{
+        const games = await Game.find()
+        res.status(200).json({message:"Success",data: games})
+    }
+    catch(error)
+    {
+        res.status(404).json({error:"Not Found"})
+    }
+})
+
+
+app.get('/book/:id',async (req,res) =>{
+    try{
+        const data = await fetchBookById(req.params.id)
+        res.status(200).json({message:"Success",data:data})
+    } catch(error)
+    {
+        console.log(error)
+        res.status(404).json({error:"Not Found"})
+    }
+})
+app.get('/book',async (req,res) =>{
+    try{
+        const games = await Game.find()
+        res.status(200).json({message:"Success",data: games})
+    }
+    catch(error)
+    {
+        res.status(404).json({error:"Not Found"})
+    }
+})
+
+
 
 //funciton to push data
 // async function createData(data) {
